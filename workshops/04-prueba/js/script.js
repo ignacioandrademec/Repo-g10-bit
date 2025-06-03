a738fc5e0894e526af4cf0d6b60700cd055def0b
Jose Andrade <ignacio.andrademec@gmail.com>
2025-06-02 21:22:53 -0500

    clase 26 de mayo

--- workshops/04-prueba/js/script.js
+++ workshops/04-prueba/js/script.js
@@ -4,6 +4,8 @@ const $p2 = document.getElementById("p2");
 const $l1 = document.getElementById("l1");
 const $l2 = document.getElementById("l2");
 const $f1 = document.getElementById("f1");
+const $f2 = document.getElementById("f2");
+const $b1 = document.getElementById("b1");
 
 const miVariableConstante = "Contenido de la variable constante, desde JS"
 const miArreglo = ["lista", "desde", "JavaScript"];
@@ -49,3 +51,27 @@ fetch("https://random-d.uk/api/v2/random")
 // PROMESAS: Son un tipo de función, que nos permite hacer manejo de acciones que se toman un tiempo, ya que todo no pasa de inmediato. 
 
 /*PROMISE:Tiene 3 estados, 1) Pending o Pendiente, 2) Resolve o Resuelta, 3) Reject o Rechazo*/
+
+
+/* OTRA MANERA EN LA QUE PODEMOS UTILIZAR APPIS DE 3ROS*/
+/* Funciones Asíncronas */
+
+/* Try es como intente hacer algo y si ocurre un error en el proceso el va a caer en catch*/
+
+$b1.addEventListener("click", ()=>{
+    obtenerInfo();
+});
+
+async function obtenerInfo() {
+    // console.log("boton clickeado, respuesta de la función");
+    try {
+        let response = await fetch("https://dog.ceo/api/breeds/image/random");
+        const data = await response.json();
+        console.log("data: ", data);
+        $f2.innerHTML = `<img src="${data.message}" alt="image">
+            <figcaption>${data.status}</figcaption>` ;
+    } catch (error) {
+        alert("Ha ocurrido un error por favor intente mas tarde")
+        console.log("error: ",error)
+    }
+}
